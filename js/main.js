01db10dccca4f874f928bcaabee3ab879c7d492d
Felipe Ostos <felipe.ostosb@istpargentina.edu.pe>
2026-06-22 16:49:45 -0500

    feat(plantilla base)

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header');
+
+    if (!header) return;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('header-scrolled');
+        } else {
+            header.classList.remove('header-scrolled');
+        }
+    });
+});
